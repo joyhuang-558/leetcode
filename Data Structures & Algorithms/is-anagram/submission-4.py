13e5960e6c397972ced892778a692e88d29d862d
Joy Huang <123406550+joyhuang-558@users.noreply.github.com>
2026-09-02 20:47:34 -0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        res_s = self.set_s(s)
+        res_t = self.set_s(t)
+        return res_s==res_t
+
+    def set_s(self,s):
+        res = set()
+        for c in s:
+            res.add(c)
+        return res
+    
+
+    
+        

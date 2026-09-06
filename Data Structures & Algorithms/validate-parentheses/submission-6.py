e7e216416e45ce1751ecc7a359ba3a39319f7c4a
Joy Huang <123406550+joyhuang-558@users.noreply.github.com>
2026-09-06 17:28:13 -0400

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        map_dic = {
+            ']':'[',
+            ')':'(',
+            '}':'{'
+        }
+        q = []
+        #如果右括号，不要加进去，pop最后一个出来match
+        #如果左括号，直接加进去
+        for a in s:
+            
+            if a not in map_dic:
+                
+                q.append(a)
+
+            else:
+ 
+                if len(q)==0:
+                    return False
+                cur = q.pop()
+
+                if map_dic[a]!=cur:
+
+                    return False
+
+        return True if len(q)==0 else False
+
+
+        
